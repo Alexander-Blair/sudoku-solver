80852c358f0776907eddec2780b208cb004b1610
Alex Blair <alex.blair@fundingcircle.com>
2019-12-24 17:48:49 +0000

    Reduce amount of recalculation for checking known values

--- ruby/lib/filters/remove_known_values.rb
+++ ruby/lib/filters/remove_known_values.rb
@@ -3,16 +3,18 @@
 module Filters
   class RemoveKnownValues
     def call(related_boxes)
+      known_values = known_values_for(related_boxes)
+
       related_boxes.each do |box_values|
         next if box_values.count == 1
 
-        known_values(related_boxes).each { |value| box_values.delete(value) }
+        known_values.each { |value| box_values.delete(value) }
       end
     end
 
     private
 
-    def known_values(related_boxes)
+    def known_values_for(related_boxes)
       related_boxes
         .select { |box_values| box_values.count == 1 }
         .map(&:first)
