2fd66c8fcadf4f214cd171a2b9614975f01d3124
Alex Blair <alex.blair@fundingcircle.com>
2020-01-03 00:09:51 +0000

    Correctly deep clone nested array

--- javascript/src/generateNextBranches.js
+++ javascript/src/generateNextBranches.js
@@ -1,4 +1,8 @@
 const generateNextBranches = (currentBranch) => {
+  const clone = (array) => array.map((boxes) => {
+    return boxes.map((possibleValue) => possibleValue);
+  });
+
   const indexToBranchFrom = currentBranch.reduce((boxToBranchFrom, box, i) => {
     if (box.length > 1) {
       if (!boxToBranchFrom || box.length < boxToBranchFrom[0].length) {
@@ -10,7 +14,7 @@ const generateNextBranches = (currentBranch) => {
   }, null)[1];
 
   return currentBranch[indexToBranchFrom].map((value) => {
-    const newBranch = currentBranch.slice();
+    const newBranch = clone(currentBranch);
     newBranch[indexToBranchFrom] = [value];
     return newBranch;
   });
